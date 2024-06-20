df2d0429babf19e11e1d78fc3c490773f44f98ef
lubna-sensegrass <lubnafathima275@gmail.com>
2024-06-20 18:31:58 +0530

    feat: added suggestedVideo component

--- src/components/SuggestedVideo/SuggestedVideo.jsx
+++ src/components/SuggestedVideo/SuggestedVideo.jsx
@@ -3,8 +3,13 @@ import './SuggestedVideo.css';
 
 const SuggestedVideo = () => {
   return (
-    <div>
-      
+    <div className="suggestion_container">
+      <div className="suggested_preview"></div>
+      <div className="suggested_content">
+        <p className="suggested_about"></p>
+        <p className="suggested_info"></p>
+        <p className="suggested_more"></p>
+      </div>
     </div>
   )
 }
